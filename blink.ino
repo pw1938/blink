387a3d70d76485e1e69a2c0597e3fe63aa8150ab
Peter Weidner <pw1938@gmx.de>
2018-08-20 11:35:53 +0200

    pw1938:three seconds is better

--- blink.ino
+++ blink.ino
@@ -3,6 +3,8 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
  
   This example code is in the public domain.
+
+  changed by pw1938@gmx.de
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
